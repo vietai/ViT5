2f2ff45c6d7e62858e01e9ffc2795ae0d422f9b8
Long Phan <44376091+justinphan3110@users.noreply.github.com>
2023-02-05 18:07:20 -0500

    Delete test.py

--- test.py
+++
@@ -1,2 +0,0 @@
-from kora.selenium import wd
-wd.get('''https://api-mainnet.magiceden.io/rpc/getListedNFTsByQuery?q={"$match":{"collectionSymbol":"degods"},"$sort":{"takerAmount":1},"$skip":0,"$limit":1}''')
